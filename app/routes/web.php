3087c6d25b82a79f759456ceab20f3d87647cc58
yugo <kawashiri.yugo.u9@s.mail.nagoya-u.ac.jp>
2024-01-07 13:26:30 +0900

    Initial commit

--- app/routes/web.php
+++ app/routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// ここから追加
+Route::resource('todo', 'App\Http\Controllers\TodoController');
+Route::put('todo/done/{todo}', [App\Http\Controllers\TodoController::class, 'done'])->name('todo.done');
+Route::put('todo/undone/{todo}', [App\Http\Controllers\TodoController::class, 'undone'])->name('todo.undone');
+//　ここまで追加
